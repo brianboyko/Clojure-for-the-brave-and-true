d7c455b852fa0428702ca1b1211b0f3569d716cc
Brian Boyko <brian.boyko@gmail.com>
2016-03-12 12:36:28 -0600

    Hello World!

---
+++ clojure-noob/project.clj
@@ -0,0 +1,5 @@
+(defproject app "1.0.0-SNAPSHOT"
+  :description "FIXME: write description"
+  :dependencies [[org.clojure/clojure "1.3.0"]]
+  ;; this will set core.clj as the main function
+  :main app.core)
---
+++ clojure-noob/src/app/core.clj
@@ -0,0 +1,4 @@
+(ns app.core)
+
+(defn -main "I don't do a whole lot...yet." [& args]
+  println "I'm a little teapot!" )
